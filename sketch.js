be0900277802ff27d4b3037d6c3bac32a7c41129
LauroLeon <98426699+LauroLeon@users.noreply.github.com>
2022-01-31 11:33:35 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,38 @@
-var box;
+
 function setup() {
   createCanvas(400,400);
+  background(51);
   box = createSprite(200,200,30,30);
-  box.shapeColor = "purple";
+
 }
 
 function draw() 
 {
-  background("orange");
-  if(keyIsDown(RIGHT_ARROW))
-  {
-box.position.x = box.position.x +5;
-background("red");
-  }
-  if(keyIsDown(LEFT_ARROW)){
-  box.position.x = box.position.x -5;
-  background("green");
-}
-if(keyIsDown(UP_ARROW)){
-box.position.y = box.position.y -5;
-background("blue");
-}
-if(keyIsDown(DOWN_ARROW)){
-box.position.y = box.position.y +5;
-background("yellow");
-}
-  drawSprites();
 
-    
+  // escribir el código para cambiar el color del fondo 
+  // rojo cuando se presiona RIGHT_ARROW (tecla de flecha derecha)
+  
   
 
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
 }
-
-
-
 
